b638d3e1cb9297c6d1c24e200dc84375373e4e8e
hhh <hhh@192.168.0.7>
2026-04-17 20:53:09 +0300

    change

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,10 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,9 +16,42 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	str := strings.Split(data, ",")
+
+	if len(str) < 2 {
+		return 0, 0 * time.Hour, errors.New("неправильный формат строки")
+	}
+
+	steps, err := strconv.Atoi(str[0])
+	if err != nil {
+		return 0, 0 * time.Hour, err
+	}
+
+	t, err := time.ParseDuration(str[1])
+
+	if err != nil {
+		return 0, 0 * time.Hour, err
+	}
+
+	return steps, t, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, _, err := parsePackage(data)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	if steps <= 0 {
+		return ""
+	}
+
+	distance := (float64(steps) * stepLength) / mInKm
+
+	//burnt := WalkingSpentCalories()
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", steps, distance, 0.00)
+
 }
